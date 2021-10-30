417de4e8a835ab48fb6599e2b72031ed2c4974dc
Andy <andyjhorn@gmail.com>
2021-10-30 15:30:31 -0500

    syntax fixes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operator/filter';
 import { map, tap } from 'rxjs/operators';
 
 @Injectable()
